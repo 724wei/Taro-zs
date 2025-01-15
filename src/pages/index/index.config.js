2bbeb9062c0c3ec270963b34dd712d7e3e9ccdcd
wei724 <1692184791@qq.com>
2025-01-15 17:12:22 +0800

    first commit

---
+++ src/pages/index/index.config.js
@@ -0,0 +1,5 @@
+export default definePageConfig({
+  navigationBarTitleText: '首页',
+  navigationBarBackgroundColor:'#0001BB',
+  navigationBarTextStyle:'white'
+})
---
+++ src/pages/index/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <view class="index">
+    <text>{{ msg }}</text>
+  </view>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import './index.scss'
+const msg = ref('周深')
+
+
+</script>
